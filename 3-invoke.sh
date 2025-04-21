abd289457f259dd5233c063446e4f89dc1151022
Ricardo A. Ferrari <ricardoaugusto.ferrari@gmail.com>
2025-04-21 20:06:42 -0300

    fix: config invokation

--- 3-invoke.sh
+++ 3-invoke.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eo pipefail
-FUNCTION=$(aws cloudformation describe-stack-resource --stack-name blank-go --logical-resource-id function --query 'StackResourceDetail.PhysicalResourceId' --output text --profile localstack)
+FUNCTION=$(aws cloudformation describe-stack-resource --stack-name blank-go --logical-resource-id function --query 'StackResourceDetail.PhysicalResourceId' --output text)
 
 while true; do
   aws lambda invoke --function-name $FUNCTION --payload fileb://event.json out.json --profile localstack
