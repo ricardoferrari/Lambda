227911cdb0f82c98d7085be9d03806ce72d0a287
Ricardo A. Ferrari <ricardoaugusto.ferrari@gmail.com>
2025-04-27 22:19:00 -0300

    fix: lambda policies

--- 4-cleanup.sh
+++ 4-cleanup.sh
@@ -5,8 +5,8 @@ if [[ $# -eq 1 ]] ; then
     STACK=$1
     echo "Deleting stack $STACK"
 fi
-FUNCTION=$(aws cloudformation describe-stack-resource --stack-name $STACK --logical-resource-id function --query 'StackResourceDetail.PhysicalResourceId' --output text)
-aws cloudformation delete-stack --stack-name $STACK
+FUNCTION=$(aws cloudformation describe-stack-resource --stack-name $STACK --logical-resource-id function --query 'StackResourceDetail.PhysicalResourceId' --output text --profile localstack)
+aws cloudformation delete-stack --stack-name $STACK --profile localstack
 echo "Deleted $STACK stack."
 
 if [ -f bucket-name.txt ]; then
@@ -17,7 +17,7 @@ if [ -f bucket-name.txt ]; then
         while true; do
             read -p "Delete deployment artifacts and bucket ($ARTIFACT_BUCKET)? (y/n)" response
             case $response in
-                [Yy]* ) aws s3 rb --force s3://$ARTIFACT_BUCKET; rm bucket-name.txt; break;;
+                [Yy]* ) aws s3 rb --force s3://$ARTIFACT_BUCKET --profile localstack; rm bucket-name.txt; break;;
                 [Nn]* ) break;;
                 * ) echo "Response must start with y or n.";;
             esac
@@ -28,7 +28,7 @@ fi
 while true; do
     read -p "Delete function log group (/aws/lambda/$FUNCTION)? (y/n)" response
     case $response in
-        [Yy]* ) aws logs delete-log-group --log-group-name /aws/lambda/$FUNCTION; break;;
+        [Yy]* ) aws logs delete-log-group --log-group-name /aws/lambda/$FUNCTION --profile localstack; break;;
         [Nn]* ) break;;
         * ) echo "Response must start with y or n.";;
     esac
